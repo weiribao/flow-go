54ea159e69c4abfc2573329d5af0f13ed17f5564
Quantaf1re <37821211+quantaf1re@users.noreply.github.com>
2019-12-20 17:26:59 -0800

    Initial commit

--- engine/consensus/HotStuff/components/voter/voter.go
+++ engine/consensus/HotStuff/components/voter/voter.go
@@ -1,7 +1,10 @@
 package voter
 
 import (
+	"github.com/dapperlabs/flow-go/engine/consensus/HotStuff/modules/crypto"
 	"github.com/dapperlabs/flow-go/engine/consensus/HotStuff/modules/def"
+	"github.com/dapperlabs/flow-go/engine/consensus/HotStuff/modules/defConAct"
+	"github.com/dapperlabs/flow-go/protocol"
 	"go.uber.org/atomic"
 )
 
@@ -9,14 +12,21 @@ import (
 // Specifically, Voter processes on OnSafeBlock events from reactor/core/eventprocessor
 type Voter struct {
 	// enable or disable Voter, e.g. for when catching up blocks
-	enabled atomic.Bool
+	enabled    atomic.Bool
+	view       uint64
+	blockKnown map[string]bool
+	// state      protocol.State
+	// TODO: Voter needs: SelfID (e.me in Max's Coldstuff) and PrimaryForView (that builds
+	// on top of protocol.state ) interfaces
+	// BlockProducer needs: SelfID and protocol.state
 }
 
-func NewVoter(enabled bool) *Voter {
-	return &Voter{enabled: *atomic.NewBool(enabled)}
+func NewVoter(enabled bool, state protocol.State) *Voter {
+	return &Voter{*atomic.NewBool(enabled), 0, make(map[string]bool), state}
 }
 
-func (v *Voter) Enable()  { v.enabled.Store(true) }
+func (v *Voter) Enable() { v.enabled.Store(true) }
+
 func (v *Voter) Disable() { v.enabled.Store(false) }
 
 // OnSafeBlock votes once Reactor has deemed that a block should be voted on
@@ -29,9 +39,18 @@ func (v *Voter) vote(block *def.Block) {
 	if !v.enabled.Load() {
 		return
 	}
-	panic("Implement rest")
+	nextPrimaryID := v.net.GetNextPrimaryAddress()
+	vote := v.GenerateAndSignVote(block, v.conIDTable.GetSelfIdx())
+	v.net.SendVote(nextPrimaryAddress, vote)
+}
+
+func (v *Voter) GenerateAndSignVote(block *def.Block, selfIdx uint32) *defConAct.Vote {
+	voteSig := crypto.SignMsg(block.BlockMRH, selfIdx)
+	vote := defConAct.NewVote(block.View, block.BlockMRH, voteSig)
+
+	return vote
 }
 
 func (v *Voter) OnEnteringView(view uint64) {
-	panic("Implement me")
+	v.view = view
 }
